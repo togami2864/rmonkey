074c1305d178aa0b56c5222dc964c81500ce23b1
togami2864 <tuabobo123@gmail.com>
2023-04-16 19:16:39 +0900

    chore: fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,80 +7,27 @@ use rmonkey_parser::Parser;
 fn main() {
     println!("Welcome to Monkey");
 
-    match std::env::args().nth(1) {
-        Some(val) => {
-            if val == "lexer" {
-                println!("lexer mode");
-                loop {
-                    let mut buffer = String::new();
-                    let stdin = io::stdin();
-                    print!(">>> ");
-                    io::stdout().flush().expect("Unable to flush stdout");
-                    stdin
-                        .read_line(&mut buffer)
-                        .expect("Unable to read line from user");
-                    match buffer.trim() {
-                        ".quit" => {
-                            println!("Bye!");
-                            std::process::exit(0);
-                        }
-                        input => {
-                            let mut l = Lexer::new(input);
-                            l.tokenize();
-                        }
-                    }
-                }
-            } else if val == "parser" {
-                println!("parser mode");
-                loop {
-                    let mut buffer = String::new();
-                    let stdin = io::stdin();
-                    print!(">>> ");
-                    io::stdout().flush().expect("Unable to flush stdout");
-                    stdin
-                        .read_line(&mut buffer)
-                        .expect("Unable to read line from user");
-                    match buffer.trim() {
-                        ".quit" => {
-                            println!("Bye!");
-                            std::process::exit(0);
-                        }
-                        input => {
-                            let l = Lexer::new(input);
-                            let mut p = Parser::new(l);
-                            p.parse();
-                        }
-                    }
-                }
-            } else {
-                println!("no such a command: {val}")
+    let mut e = Evaluator::new();
+    loop {
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        print!(">>> ");
+        io::stdout().flush().expect("Unable to flush stdout");
+        stdin
+            .read_line(&mut buffer)
+            .expect("Unable to read line from user");
+        match buffer.trim() {
+            ".quit" => {
+                println!("Bye!");
+                std::process::exit(0);
             }
-        }
-
-        None => {
-            let mut e = Evaluator::new();
-            loop {
-                let mut buffer = String::new();
-                let stdin = io::stdin();
-                print!(">>> ");
-                io::stdout().flush().expect("Unable to flush stdout");
-                stdin
-                    .read_line(&mut buffer)
-                    .expect("Unable to read line from user");
-                match buffer.trim() {
-                    ".quit" => {
-                        println!("Bye!");
-                        std::process::exit(0);
-                    }
-                    input => {
-                        let l = Lexer::new(input);
-                        let mut p = Parser::new(l);
-                        let program = p.parse_program().unwrap();
-                        match e.eval(program) {
-                            Ok(result) => println!("{result}"),
-                            Err(err) => eprintln!("{err}"),
-                        }
-                    }
+            input => {
+                let l = Lexer::new(input);
+                let mut p = Parser::new(l);
+                let program = p.parse_program().unwrap();
+                match e.eval(program) {
+                    Ok(result) => println!("{result}"),
+                    Err(err) => eprintln!("{err}"),
                 }
             }
         }
