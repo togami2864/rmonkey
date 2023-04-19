f7fe1a80c0b17903307a9e7f62aa7af271ce13b1
togami2864 <tuabobo123@gmail.com>
2023-04-20 00:43:55 +0900

    chore: configure prettier eslint

--- playground/app.vue
+++ playground/app.vue
@@ -1,67 +1,91 @@
 <template>
-<div class="app">
-  <header>
-    <button class="button__run">Run</button>
-  </header>
-  <div class="editor"></div>
-  <div class="ast"></div>
-  <div class="console"></div>
-  <footer></footer>
-</div>
+    <div class="app">
+        <header>
+            <button class="button__run">Run</button>
+        </header>
+        <div class="editor"></div>
+        <div class="ast"></div>
+        <div class="console">
+            <p class="result">Result</p>
+        </div>
+        <footer></footer>
+    </div>
 </template>
 
 <script lang="ts">
-import {output_log} from "rmonkey_wasm"
-output_log("hello")
+import { output_log } from "rmonkey_wasm";
+output_log("hello");
 </script>
 
 <style scoped>
-/* .app {
-    display: grid;
-    grid-template-rows: 60px calc(100% - 290px) 194px 36px;
-    height: 100vh;
-    min-height: 500px;
-    width: 100vw
-} */
-.container{
-background-color: black;
+.container {
+    background-color: black;
 }
-.header{
+.header {
     justify-content: space-between;
 }
 
 .app {
-  height: 100vh;
-display: grid;
-grid-template-columns: repeat(2, 1fr);
-grid-template-rows: 0.7fr repeat(4, 1fr) 0.5fr;
-grid-column-gap: 0px;
-grid-row-gap: 0px;
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 0.4fr repeat(4, 1fr) 0.3fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
 }
 
-.header { grid-area: 1 / 1 / 2 / 6; }
-.editor { grid-area: 2 / 1 / 5 / 2; }
-.ast { grid-area: 2 / 2 / 5 / 3; }
-.console { grid-area: 5 / 1 / 6 / 3; }
-.footer { grid-area: 6 / 1 / 7 / 3; }
+header {
+    padding: 10px 20px;
+    background-color: black;
+    grid-area: 1 / 1 / 2 / 6;
+    display: flex;
+    justify-content: space-between;
+}
+.editor {
+    background-color: #171417;
+    grid-area: 2 / 1 / 5 / 2;
+    border-bottom: 1px solid white;
+    border-right: 1px solid white;
+
+}
+.ast {
+    background-color: #171417;
+    grid-area: 2 / 2 / 5 / 3;
+    border-bottom: 1px solid white;
+
+}
+.console {
+    background-color: #171417;
+    grid-area: 5 / 1 / 6 / 3;
+}
+footer {
+    background-color: black;
+    grid-area: 6 / 1 / 7 / 3;
+}
+
+.result{
+    color: white;
+    font-weight: bold;
+    padding: 10px;
+}
 
 .button__run {
- background-color: black;
-  color: white;
-  padding: 10px 20px;
-  border-radius: 20px;
-  border: none;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  cursor: pointer;
+    background-color: black;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 20px;
+    border: solid 2px white;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    cursor: pointer;
 }
 .button__run:hover {
-  background-color: white;
-  color: black;
-  transition: 500ms;
+    background-color: white;
+    color: black;
+    transition: 500ms;
 }
-
 </style>
